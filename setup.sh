ce31763afb5041e8f0d67d7da8b758bdc39c1337
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2024-01-09 23:56:59 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ port = $PORT\n\
 [theme]\n\
 primaryColor = '#d33682'\n\
 backgroundColor = '#002b36'\n\
-secondaryBackgroundColor = '#586e75'\n\
+
 textColor = '#fff'\n\
 " > ~/.streamlit/config.toml
